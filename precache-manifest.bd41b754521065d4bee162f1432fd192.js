3a8ea3e5c64a432ec95d022a627614f240ab6b2d
Oscar Sinuco <oscarsinuco@gmail.com>
2020-07-07 18:39:35 -0500

    Updates

---
+++ precache-manifest.bd41b754521065d4bee162f1432fd192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30abe9e9e04a792b119082a2c85c20c2",
+    "url": "/reproductor/index.html"
+  },
+  {
+    "revision": "31032b98b12c8080ded2",
+    "url": "/reproductor/static/css/main.763277a3.chunk.css"
+  },
+  {
+    "revision": "3704c5275dd4e9eaf126",
+    "url": "/reproductor/static/js/2.5af5d324.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reproductor/static/js/2.5af5d324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31032b98b12c8080ded2",
+    "url": "/reproductor/static/js/main.97303b4c.chunk.js"
+  },
+  {
+    "revision": "5d7188a0428bd73f8f3e",
+    "url": "/reproductor/static/js/runtime-main.09e6cf9b.js"
+  },
+  {
+    "revision": "f904b640e5b6c5064fc2e7217d422901",
+    "url": "/reproductor/static/media/headphones.f904b640.png"
+  }
+]);
